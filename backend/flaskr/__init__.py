e18a461ac6b58d4fa3f9c42002326086cbfe5b7e
Burak Tilek <burak.tilek@gmail.com>
2020-04-11 23:14:55 +0300

    /questions/:id delete endpoint is implemented

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-import random
+import random, sys
 from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
@@ -68,13 +68,22 @@ def create_app(test_config=None):
     })
 
 
-  '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
+  @app.route('/questions/<int:question_id>', methods=['DELETE'])
+  def delete_question(question_id):
+    question = Question.query.filter(Question.id == question_id).one_or_none()
 
-  TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
-  '''
+    try:
+      if question is None:
+        abort(404)
+      else:
+        question.delete()
+
+        return jsonify({
+          'success': True,
+        })
+
+    except:
+      abort(422)
 
   '''
   @TODO: 
