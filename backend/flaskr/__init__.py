5d27a9293f3bcb991eabdd04382fb40444ac187e
Burak Tilek <burak.tilek@gmail.com>
2020-04-12 13:23:08 +0300

    Created error handlers for all expected errors

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -192,10 +192,37 @@ def create_app(test_config=None):
     except:
       abort(422)
 
-  '''
-  @TODO: 
-  Create error handlers for all expected errors 
-  including 404 and 422. 
-  '''
+
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+      'success': False,
+      'error': 404,
+      'message': 'Not found',
+    }), 404
+
+  @app.errorhandler(422)
+  def unproccesable(error):
+    return jsonify({
+      'success': False,
+      'error': 422,
+      'message': 'Unproccesable entity',
+    }), 422
+
+  @app.errorhandler(400)
+  def bad_request(error):
+    return jsonify({
+      'success': False,
+      'error': 400,
+      'message': 'Bad request',
+    }), 400
+
+  @app.errorhandler(405)
+  def not_allowed(error):
+    return jsonify({
+      'success': False,
+      'error': 405,
+      'message': 'Method not allowed',
+    })
 
   return app
